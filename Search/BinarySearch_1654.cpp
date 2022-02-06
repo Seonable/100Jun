8c11df8d40254e95b1030cd94a31ccb5fa8bacca
OOS-SQUARE <sun93251@naver.com>
2022-02-07 03:28:33 +0900

    최적화 문제를 결정문제로

--- Search/BinarySearch_1654.cpp
+++ Search/BinarySearch_1654.cpp
@@ -1,4 +1,34 @@
-//
-// Created by 이선열 on 2022/02/07.
-//
+#include <bits/stdc++.h>
+using namespace std;
 
+int K;
+int N;
+int lineLength[10001];
+
+bool solve(int length) {
+    int cur = 0;
+    for (int i = 0; i < K; i++) {
+        cur += lineLength[i] / length;
+    }
+    return cur >= N;
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> K >> N;
+    for (int i = 0; i < K; i++) cin >> lineLength[i];
+
+    long long start = 0, end = 0x7fffffff;
+
+    while (start < end) {
+        long long mid = (start + end + 1) / 2;
+
+        if (solve(mid)) start = mid;
+        else end = mid - 1;
+    }
+    cout << start;
+    return 0;
+}
